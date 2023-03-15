a72d2ea58b00dfe93b783b0a7c4d53e770f13608
Srinidhi N <srinidhin@outlook.com>
2023-03-15 16:21:53 +0100

    Completed property guided policy

--- policies/property_guided.go
+++ policies/property_guided.go
@@ -2,176 +2,227 @@ package policies
 
 import (
 	"math"
+	"math/rand"
+	"time"
 
 	"github.com/zeu5/raft-rl-test/types"
 	"gonum.org/v1/gonum/stat/sampleuv"
 )
 
-type PropertyGuidedPolicy struct {
-	properties []*types.Monitor
-	QTableExp   map[string]map[string]float64
-	QTablesProp	[]map[string]map[string]float64
-	alpha    float64
-	gamma    float64
-	epsilon	float64 // probability to still follow general Exploration instead of prop, greedy over QTablesProp[prop] otherwise
+type QTable struct {
+	table map[string]map[string]float64
+}
 
-	episodes int // keep track of total number of episodes 
-	prop_episodes []int // keep track of number of episodes each property has been followed
-	current_prop int // just an index for which property is being followed, 0 => no property
+func NewQTable() *QTable {
+	return &QTable{
+		table: make(map[string]map[string]float64),
+	}
+}
+
+func (q *QTable) Get(state, action string, def float64) float64 {
+	if _, ok := q.table[state]; !ok {
+		q.table[state] = make(map[string]float64)
+	}
+	if _, ok := q.table[state][action]; !ok {
+		q.table[state][action] = def
+	}
+	return q.table[state][action]
+}
+
+func (q *QTable) Set(state, action string, val float64) {
+	if _, ok := q.table[state]; !ok {
+		q.table[state] = make(map[string]float64)
+	}
+	if _, ok := q.table[state][action]; !ok {
+		q.table[state][action] = val
+	}
+}
+
+func (q *QTable) HasState(state string) bool {
+	_, ok := q.table[state]
+	return ok
+}
+
+func (q *QTable) Max(state string, def float64) (string, float64) {
+	if _, ok := q.table[state]; !ok {
+		q.table[state] = make(map[string]float64)
+		return "", def
+	}
+	maxAction := ""
+	maxVal := float64(math.MinInt)
+	for a, val := range q.table[state] {
+		if val > maxVal {
+			maxAction = a
+			maxVal = val
+		}
+	}
+	if maxAction == "" {
+		return "", def
+	}
+	return maxAction, maxVal
+}
+
+func (q *QTable) MaxAmong(state string, actions []string, def float64) (string, float64) {
+	if _, ok := q.table[state]; !ok {
+		q.table[state] = make(map[string]float64)
+	}
+	maxAction := ""
+	maxVal := float64(math.MinInt)
+	for _, a := range actions {
+		if _, ok := q.table[state][a]; !ok {
+			q.table[state][a] = def
+		}
+		val := q.table[state][a]
+		if val > maxVal {
+			maxAction = a
+			maxVal = val
+		}
+	}
+	return maxAction, maxVal
+}
+
+type PropertyGuidedPolicy struct {
+	properties  []*types.Monitor
+	expQTable   *QTable
+	propQTables []*QTable
+	alpha       float64
+	gamma       float64
+	// probability to still follow general Exploration instead of prop, greedy over QTablesProp[prop] otherwise
+	epsilon float64
+
+	// keep track of total number of episodes
+	episodes int
+	// keep track of number of episodes each property has been followed
+	propEpisodes []int
+	// just an index for which property is being followed, 0 => no property
+	currentProp int
+	rand        *rand.Rand
 }
 
 var _ types.Policy = &PropertyGuidedPolicy{}
 
 func NewPropertyGuidedPolicy(properties []*types.Monitor, alpha, gamma, epsilon float64) *PropertyGuidedPolicy {
-	return &PropertyGuidedPolicy{
-		properties: []properties,
-		QTableExp:   make(map[string]map[string]float64), // general exploration table, update it all the times, use it when not following any prop or state is unknown for the prop
-		QTablesProp: []make(map[string]map[string]float64), // list of QTables, one for each prop, updated only for trajectories that fulfill the prop
-		alpha:    alpha,
-		gamma:    gamma,
-		epsilon:	epsilon,
-
-		episodes := 0,
-		current_prop := -1, // start with no property
+	policy := &PropertyGuidedPolicy{
+		properties: properties,
+		// general exploration table, update it all the times, use it when not following any prop or state is unknown for the prop
+		expQTable: NewQTable(),
+		// list of QTables, one for each prop, updated only for trajectories that fulfill the prop
+		propQTables: make([]*QTable, len(properties)),
+		alpha:       alpha,
+		gamma:       gamma,
+		epsilon:     epsilon,
+		// start with no property
+		currentProp: -1,
+		rand:        rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
+	for i := range properties {
+		policy.propQTables[i] = NewQTable()
+	}
+	return policy
 }
 
 // Run at the end of each episode, use the trace to update policies and choose next property
 func (p *PropertyGuidedPolicy) UpdateIteration(iteration int, trace *types.Trace) {
-	for i := 0; i < properties.Len(); i++ { // check and update policies for all properties
-		p.updatePolicy(properties[i], trace)
+	// check and update policies for all properties
+	for i := 0; i < len(p.properties); i++ {
+		p.updatePolicy(i, trace)
 	}
 
-	p.choose_property(iteration) // choose prop to follow in next episode
+	// choose prop to follow in next episode
+	p.chooseProperty(iteration)
+
+	p.episodes += 1
 }
 
 // Choose which property to use next, according to some policy
-func (p *PropertyGuidedPolicy) choose_property(iteration int) {
-	if p.current_prop == (p.properties.Len() - 1) {
-		p.current_prop = -1
+func (p *PropertyGuidedPolicy) chooseProperty(iteration int) {
+	if p.currentProp == (len(p.properties) - 1) {
+		p.currentProp = -1
 	} else {
-		p.current_prop += 1
+		p.currentProp += 1
 	}
 }
 
 // Takes a property and a trace and updates the Qvalues backwards giving reward 1 for the
 // last (state,action) pair.
-func (p *PropertyGuidedPolicy) updatePolicy(prop property, trace *types.Trace) {
-	prefix, ok := prop.Check(trace) // last (s,a,s') should be where s' fulfills the property.
+func (p *PropertyGuidedPolicy) updatePolicy(propertyIndex int, trace *types.Trace) {
+	prop := p.properties[propertyIndex]
+	// last (s,a,s') should be where s' fulfills the property.
+	prefix, ok := prop.Check(trace)
 	if !ok {
 		return
 	}
 
+	p.propEpisodes[propertyIndex] += 1
+	propQTable := p.propQTables[propertyIndex]
 	for i := prefix.Len() - 1; i >= 0; i-- {
 		state, action, nextState, _ := prefix.Get(i)
 		stateHash := state.Hash()
 
 		nextStateHash := nextState.Hash()
 		actionKey := action.Hash()
-		if _, ok := p.QTable[stateHash]; !ok { // TO UPDATE, QTable of the property QTablesProp[i]
-			return // create a new map
-		}
-		if _, ok := p.QTable[stateHash][actionKey]; !ok {
-			return // init to 0
-		}
-		curVal := p.QTable[stateHash][actionKey]
-		max := float64(0)
-		if _, ok := p.QTable[nextStateHash]; ok {
-			for _, val := range p.QTable[nextStateHash] {
-				if val > max {
-					max = val
-				}
-			}
+		curVal := propQTable.Get(stateHash, actionKey, 0)
+		_, max := propQTable.Max(nextStateHash, 0)
+
+		var nextVal float64
+		if i == prefix.Len()-1 {
+			// last step, give 1 reward and 0 for next state
+			nextVal = (1-p.alpha)*curVal + p.alpha*(1+p.gamma*max)
+		} else {
+			// otherwise, update with zero reward + V(nextState)
+			nextVal = (1-p.alpha)*curVal + p.alpha*(0+p.gamma*max)
 		}
 
-		if i == prefix.Len() - 1 { // last step, give 1 reward and 0 for next state
-			nextVal := (1-p.alpha)*curVal + p.alpha*(1 + p.gamma*max)
-		} else { // otherwise, update with zero reward + V(nextState)
-			nextVal := (1-p.alpha)*curVal + p.alpha*(0 + p.gamma*max)
-		}
-		
-		p.QTable[stateHash][actionKey] = nextVal
+		propQTable.Set(stateHash, actionKey, nextVal)
 	}
 }
 
 func (p *PropertyGuidedPolicy) NextAction(step int, state types.State, actions []types.Action) (types.Action, bool) {
 	stateHash := state.Hash()
 
-	if p.current_prop != -1 { // there is a selected prop policy
-		if _, ok := p.QTablesProp[p.current_prop][stateHash]; !ok { // state unkown for the prop policy, just follow exploration
+	if p.currentProp != -1 {
+		propQTable := p.propQTables[p.currentProp]
+		// there is a selected prop policy
+		if !propQTable.HasState(stateHash) {
+			// state unkown for the prop policy, just follow exploration
 			return p.nextActionExp(stateHash, actions)
 		}
+
 		// ... TO DO ...
 		// sample random number [0,1]: if < epsilon:
 		//   use general Exploration
 		// else:
 		//   choose action greedily over QTablesProp[p.current_prop][stateHash][action]
-
-		for _, a := range actions {
-			aName := a.Hash()
-			if _, ok := p.QTable[stateHash][aName]; !ok {
-				p.QTable[stateHash][aName] = 0
-			}
+		if rand.Float64() < p.epsilon {
+			return p.nextActionExp(stateHash, actions)
 		}
-	} else { // no prop selected, use general Exploration
-		return p.nextActionExp(stateHash, actions)
-	}
-
-	if _, ok := p.QTablesProp[p.current_prop][stateHash]; !ok { // state unkown for the prop policy, just follow exploration
-		p.QTable[stateHash] = make(map[string]float64)
-	}
-	
-
-	for _, a := range actions {
-		aName := a.Hash()
-		if _, ok := p.QTable[stateHash][aName]; !ok {
-			p.QTable[stateHash][aName] = 0
+		actionMap := make(map[string]types.Action)
+		actionKeys := make([]string, len(actions))
+		for i, a := range actions {
+			actionKeys[i] = a.Hash()
+			actionMap[a.Hash()] = a
 		}
+		maxAction, _ := propQTable.MaxAmong(stateHash, actionKeys, 0)
+		if maxAction == "" {
+			return nil, false
+		}
+		return actionMap[maxAction], true
 	}
-
-	sum := float64(0)
-	weights := make([]float64, len(actions))
-	vals := make([]float64, len(actions))
-
-	for i, action := range actions {
-		val := p.QTable[stateHash][action.Hash()]
-		exp := math.Exp(val)
-		vals[i] = exp
-		sum += exp
-	}
-
-	for i, v := range vals {
-		weights[i] = v / sum
-	}
-	i, ok := sampleuv.NewWeighted(weights, nil).Take()
-	if !ok {
-		return nil, false
-	}
-	return actions[i], true
+	// no prop selected, use general Exploration
+	return p.nextActionExp(stateHash, actions)
 }
 
 // choose next action based on the general Exploration policy
 func (p *PropertyGuidedPolicy) nextActionExp(statehash string, actions []types.Action) (types.Action, bool) {
-	
-	if _, ok := p.QTableExp[stateHash]; !ok { // if never seen, create new map for the state
-		p.QTableExp[stateHash] = make(map[string]float64)
-	}
-	
-
-	for _, a := range actions { // initialize non-existant QVals to 0
-		aName := a.Hash()
-		if _, ok := p.QTableExp[stateHash][aName]; !ok {
-			p.QTableExp[stateHash][aName] = 0
-		}
-	}
 
 	// compute softmax and sample action according to the distribution
 	sum := float64(0)
 	weights := make([]float64, len(actions))
 	vals := make([]float64, len(actions))
 
-	for i, action := range actions { // better to normalize everything subtracting Max(vals) to vals
-		val := p.QTableExp[stateHash][action.Hash()]
+	for i, action := range actions {
+		// better to normalize everything subtracting Max(vals) to vals
+		val := p.expQTable.Get(statehash, action.Hash(), 0)
 		exp := math.Exp(val)
 		vals[i] = exp
 		sum += exp
@@ -188,28 +239,15 @@ func (p *PropertyGuidedPolicy) nextActionExp(statehash string, actions []types.A
 }
 
 // update the general Exploration policy, ??? it can be done at each step, or at the end of each episode ???
-func (p *PropertyGuidedPolicy) Update(step int, state State, action Action, nextState State) {
+func (p *PropertyGuidedPolicy) Update(step int, state types.State, action types.Action, nextState types.State) {
 	stateHash := state.Hash()
 
 	nextStateHash := nextState.Hash()
 	actionKey := action.Hash()
-	if _, ok := p.QTableExp[stateHash]; !ok { // if never seen, create new map for the state
-		p.QTableExp[stateHash] = make(map[string]float64)
-	}
-	if _, ok := p.QTableExp[stateHash][actionKey]; !ok { // initialize non-existant QVals to 0
-		p.QTableExp[stateHash][actionKey] = 0
-	}
-	curVal := p.QTableExp[stateHash][actionKey]
+	curVal := p.expQTable.Get(stateHash, actionKey, 0)
 	// take V(nextState)
-	max := float64(0)
-	if _, ok := p.QTableExp[nextStateHash]; ok {
-		for _, val := range p.QTableExp[nextStateHash] {
-			if val > max {
-				max = val
-			}
-		}
-	}
+	_, max := p.expQTable.Max(nextStateHash, 0)
 	// update QVal
-	nextVal := (1-p.alpha)*curVal + p.alpha*(-1+s.gamma*max)
-	p.QTableExp[stateHash][actionKey] = nextVal
+	nextVal := (1-p.alpha)*curVal + p.alpha*(-1+p.gamma*max)
+	p.expQTable.Set(stateHash, actionKey, nextVal)
 }
