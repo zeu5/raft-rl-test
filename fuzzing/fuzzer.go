b1b2523ba83595549eeaee87f5adc2c4f946fdf1
Srinidhi N <srinidhin@outlook.com>
2023-03-21 11:35:59 +0100

    Completed fuzzer for raft

--- fuzzing/fuzzer.go
+++ fuzzing/fuzzer.go
@@ -8,55 +8,65 @@ import (
 )
 
 type Fuzzer struct {
-	MessageQueues               map[uint64]*Queue[pb.Message]
-	Nodes                       []uint64
-	Config                      *FuzzerConfig
-	Mutator                     Mutator
-	MutatedNodeChoices          *Queue[uint64]
-	CurEventTrace               EventTrace
-	CurTrace                    Trace
-	MutatedRandomBooleanChoices *Queue[bool]
-	MutatedRandomIntegerChoices *Queue[int]
+	messageQueues               map[uint64]*Queue[pb.Message]
+	nodes                       []uint64
+	config                      *FuzzerConfig
+	mutator                     Mutator
+	mutatedNodeChoices          *Queue[uint64]
+	curEventTrace               *List[*Event]
+	curTrace                    *List[*SchedulingChoice]
+	mutatedRandomBooleanChoices *Queue[bool]
+	mutatedRandomIntegerChoices *Queue[int]
 	rand                        *rand.Rand
-	RaftEnvironment             *RaftEnvironment
+	raftEnvironment             *RaftEnvironment
+	tlcClient                   *TLCClient
+	statesMap                   map[int64]bool
 }
 
 type FuzzerConfig struct {
 	Iterations            int
 	Steps                 int
+	TLCAddr               string
 	Mutator               Mutator
 	RaftEnvironmentConfig RaftEnvironmentConfig
 }
 
 func NewFuzzer(config *FuzzerConfig) *Fuzzer {
-	return &Fuzzer{
-		Config:                      config,
-		Nodes:                       make([]uint64, 0),
-		MessageQueues:               make(map[uint64]*Queue[pb.Message]),
-		Mutator:                     config.Mutator,
-		MutatedNodeChoices:          NewQueue[uint64](),
-		CurEventTrace:               NewEventTrace(),
-		CurTrace:                    NewTrace(),
-		MutatedRandomBooleanChoices: NewQueue[bool](),
-		MutatedRandomIntegerChoices: NewQueue[int](),
+	f := &Fuzzer{
+		config:                      config,
+		nodes:                       make([]uint64, 0),
+		messageQueues:               make(map[uint64]*Queue[pb.Message]),
+		mutator:                     config.Mutator,
+		mutatedNodeChoices:          NewQueue[uint64](),
+		curEventTrace:               NewList[*Event](),
+		curTrace:                    NewList[*SchedulingChoice](),
+		mutatedRandomBooleanChoices: NewQueue[bool](),
+		mutatedRandomIntegerChoices: NewQueue[int](),
 		rand:                        rand.New(rand.NewSource(time.Now().UnixNano())),
-		RaftEnvironment:             NewRaftEnvironment(config.RaftEnvironmentConfig),
+		raftEnvironment:             NewRaftEnvironment(config.RaftEnvironmentConfig),
+		tlcClient:                   NewTLCClient(config.TLCAddr),
+		statesMap:                   make(map[int64]bool),
 	}
+	for i := 0; i < f.config.RaftEnvironmentConfig.Replicas; i++ {
+		f.nodes = append(f.nodes, uint64(i))
+		f.messageQueues[uint64(i)] = NewQueue[pb.Message]()
+	}
+	return f
 }
 
 func (f *Fuzzer) GetRandomBoolean() (choice bool) {
-	if f.MutatedRandomBooleanChoices.Size() > 0 {
-		choice, _ = f.MutatedRandomBooleanChoices.Pop()
+	if f.mutatedRandomBooleanChoices.Size() > 0 {
+		choice, _ = f.mutatedRandomBooleanChoices.Pop()
 	} else {
 		choice = f.rand.Intn(2) == 0
 	}
-	f.CurEventTrace = append(f.CurEventTrace, &Event{
+	f.curEventTrace.Append(&Event{
 		Name: "RandomBooleanChoice",
 		Params: map[string]interface{}{
 			"choice": choice,
 		},
 	})
-	f.CurTrace = append(f.CurTrace, &SchedulingChoice{
+	f.curTrace.Append(&SchedulingChoice{
 		Type:          RandomBoolean,
 		BooleanChoice: choice,
 	})
@@ -64,18 +74,18 @@ func (f *Fuzzer) GetRandomBoolean() (choice bool) {
 }
 
 func (f *Fuzzer) GetRandomInteger(max int) (choice int) {
-	if f.MutatedRandomIntegerChoices.Size() > 0 {
-		choice, _ = f.MutatedRandomIntegerChoices.Pop()
+	if f.mutatedRandomIntegerChoices.Size() > 0 {
+		choice, _ = f.mutatedRandomIntegerChoices.Pop()
 	} else {
 		choice = f.rand.Intn(max)
 	}
-	f.CurEventTrace = append(f.CurEventTrace, &Event{
+	f.curEventTrace.Append(&Event{
 		Name: "RandomIntegerChoice",
 		Params: map[string]interface{}{
 			"choice": choice,
 		},
 	})
-	f.CurTrace = append(f.CurTrace, &SchedulingChoice{
+	f.curTrace.Append(&SchedulingChoice{
 		Type:          RandomInteger,
 		IntegerChoice: choice,
 	})
@@ -84,18 +94,25 @@ func (f *Fuzzer) GetRandomInteger(max int) (choice int) {
 
 func (f *Fuzzer) GetNextMessage() (message pb.Message, ok bool) {
 	var nextNode uint64
-	if f.MutatedNodeChoices.Size() > 0 {
-		nextNode, _ = f.MutatedNodeChoices.Pop()
+	if f.mutatedNodeChoices.Size() > 0 {
+		nextNode, _ = f.mutatedNodeChoices.Pop()
 	} else {
-		randIndex := f.rand.Intn(len(f.Nodes))
-		nextNode = f.Nodes[randIndex]
+		availableNodes := make([]uint64, 0)
+		for node, q := range f.messageQueues {
+			if q.Size() > 0 {
+				availableNodes = append(availableNodes, node)
+			}
+		}
+		randIndex := f.rand.Intn(len(availableNodes))
+		nextNode = availableNodes[randIndex]
 	}
-	message, ok = f.MessageQueues[nextNode].Pop()
-	f.CurEventTrace = append(f.CurEventTrace, &Event{
+	message, ok = f.messageQueues[nextNode].Pop()
+	// TODO: encode all message params
+	f.curEventTrace.Append(&Event{
 		Name:   message.Type.String(),
 		Params: map[string]interface{}{},
 	})
-	f.CurTrace = append(f.CurTrace, &SchedulingChoice{
+	f.curTrace.Append(&SchedulingChoice{
 		Type:   Node,
 		NodeID: nextNode,
 	})
@@ -103,17 +120,67 @@ func (f *Fuzzer) GetNextMessage() (message pb.Message, ok bool) {
 }
 
 func (f *Fuzzer) Run() error {
-	for i := 0; i < f.Config.Iterations; i++ {
-		init := f.RaftEnvironment.Reset()
-		for _, m := range init {
-			f.MessageQueues[m.To].Push(m)
+	ctx := &FuzzContext{fuzzer: f}
+	for i := 0; i < f.config.Iterations; i++ {
+		// Reset current trace
+		f.curEventTrace.Reset()
+		f.curTrace.Reset()
+		for _, q := range f.messageQueues {
+			q.Reset()
 		}
+		init := f.raftEnvironment.Reset()
+		for _, m := range init {
+			f.messageQueues[m.To].Push(m)
+		}
+
+		for j := 0; i < f.config.Steps; j++ {
+			message, ok := f.GetNextMessage()
+			if !ok {
+				continue
+			}
+			new := f.raftEnvironment.Step(ctx, message)
+			for _, m := range new {
+				if q, ok := f.messageQueues[m.To]; ok {
+					q.Push(m)
+				}
+			}
+		}
+		f.mutatedNodeChoices.Reset()
+		f.mutatedRandomBooleanChoices.Reset()
+		f.mutatedRandomIntegerChoices.Reset()
+		// Transmit trace to TLC and mutate
+		if tlcStates, err := f.tlcClient.SendTrace(f.curEventTrace); err == nil {
+			haveNewState := false
+			for _, s := range tlcStates {
+				_, ok := f.statesMap[s.Key]
+				if !ok {
+					haveNewState = true
+					f.statesMap[s.Key] = true
+				}
+			}
+			if haveNewState {
+				mutatedTrace, ok := f.mutator.Mutate(f.curTrace, f.curEventTrace)
+				if ok {
+					for _, choice := range mutatedTrace.AsList() {
+						switch choice.Type {
+						case RandomBoolean:
+							f.mutatedRandomBooleanChoices.Push(choice.BooleanChoice)
+						case RandomInteger:
+							f.mutatedRandomIntegerChoices.Push(choice.IntegerChoice)
+						case Node:
+							f.mutatedNodeChoices.Push(choice.NodeID)
+						}
+					}
+				}
+			}
+		}
+
 	}
 	return nil
 }
 
 type Mutator interface {
-	Mutate(*Trace) (*Trace, bool)
+	Mutate(*List[*SchedulingChoice], *List[*Event]) (*List[*SchedulingChoice], bool)
 }
 
 type FuzzContext struct {
@@ -121,7 +188,7 @@ type FuzzContext struct {
 }
 
 func (f *FuzzContext) AddEvent(e *Event) {
-	f.fuzzer.CurEventTrace = append(f.fuzzer.CurEventTrace, e)
+	f.fuzzer.curEventTrace.Append(e)
 }
 
 func (f *FuzzContext) RandomBooleanChoice() bool {
--- fuzzing/types.go
+++ fuzzing/types.go
@@ -1,5 +1,26 @@
 package fuzzing
 
+type Event struct {
+	Name   string
+	Params map[string]interface{}
+	Reset  bool
+}
+
+var (
+	Node          SchedulingChoiceType = "Node"
+	RandomBoolean SchedulingChoiceType = "RandomBoolean"
+	RandomInteger SchedulingChoiceType = "RandomInteger"
+)
+
+type SchedulingChoiceType string
+
+type SchedulingChoice struct {
+	Type          SchedulingChoiceType
+	NodeID        uint64
+	BooleanChoice bool
+	IntegerChoice int
+}
+
 type Queue[T any] struct {
 	q []T
 }
@@ -21,9 +42,55 @@ func (q *Queue[T]) Pop() (elem T, ok bool) {
 	}
 	elem = q.q[0]
 	q.q = q.q[1:]
+	ok = true
 	return
 }
 
 func (q *Queue[T]) Size() int {
 	return len(q.q)
+}
+
+func (q *Queue[T]) Reset() {
+	q.q = make([]T, 0)
+}
+
+type List[T any] struct {
+	l []T
+}
+
+func NewList[T any]() *List[T] {
+	return &List[T]{
+		l: make([]T, 0),
+	}
+}
+
+func (l *List[T]) Append(elem T) {
+	l.l = append(l.l, elem)
+}
+
+func (l *List[T]) Size() int {
+	return len(l.l)
+}
+
+func (l *List[T]) Get(index int) (elem T, ok bool) {
+	if len(l.l) <= index {
+		ok = false
+		return
+	}
+	elem = l.l[index]
+	ok = true
+	return
+}
+
+func (l *List[T]) Reset() {
+	l.l = make([]T, 0)
+}
+
+func (l *List[T]) AsList() []T {
+	return l.l
+}
+
+type State struct {
+	Repr string
+	Key  int64
 }
